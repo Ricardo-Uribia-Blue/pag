dbcf2b37e9393c872653141d3d26d6bddd90b4ab
ricardo.uribia <ricardo.uribia@grupoblueocean.com>
2023-10-26 09:22:35 -0600

    package.json

---
+++ resources/views/app/files/files.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documentos</title>
+    <style type="text/css">
+        img{
+            max-width: 150px;
+        }
+    </style>
+</head>
+<body>
+
+<form action="" method="POST" enctype="multipart/form-data">
+        @csrf
+
+        <input type="file" name="file" required>
+</form>
+    
+</body>
+</html>
